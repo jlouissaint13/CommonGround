2a97dd5f0fa98d5a00b5be109b939ace503e6887
jlouissaint13 <114112722+jlouissaint13@users.noreply.github.com>
2025-04-01 03:36:27 -0400

    Start

---
+++ backend/CommonGround/src/test/java/com/commonground/CommonGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commonground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
